97f544eeee8690b70fdb3fd2e22e06a872f00155
Meg Shulmister <meganshulmister@gmail.com>
2021-01-09 11:44:52 -0700

    Styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import btc from './imgs/btc.svg';
 
 function App() {
   return (
     <div className="App">
+      <div className="btc-logo">
+        <img src={btc} alt="BTC"/>
+      </div>
       <div className="creds">
       Powered by CoinDesk
       </div>
